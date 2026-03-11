ec04e097c54fd84fdec07ebb2db039fd5570bc39
shannon shields <shannon.shields@clogin02.cactus.wcoss2.ncep.noaa.gov>
2026-03-11 20:04:37 +0000

    Add plot script for stream1a

---
+++ dev/drivers/scripts/plots/global_det/jevs_global_det_atmos_grid2grid_nwp_index_plots_last47days.sh
@@ -0,0 +1,61 @@
+#PBS -N jevs_global_det_atmos_grid2grid_nwp_index_plots_last47days_00
+#PBS -j oe
+#PBS -S /bin/bash
+#PBS -q dev
+#PBS -A VERF-DEV
+#PBS -l walltime=00:10:00
+#PBS -l place=vscatter:exclhost,select=1:ncpus=5:ompthreads=1:mem=150GB
+#PBS -l debug=true
+
+set -x
+
+cd $PBS_O_WORKDIR
+
+export model=evs
+export HOMEevs=/lfs/h2/emc/vpppg/noscrub/$USER/feature_global_det_NWP_Index/EVS
+
+export SENDCOM=YES
+export KEEPDATA=YES
+export SENDDBN=NO
+export job=${PBS_JOBNAME:-jevs_global_det_atmos_grid2grid_nwp_index_plots_last47days}
+export jobid=$job.${PBS_JOBID:-$$}
+export SITE=$(cat /etc/cluster_name)
+export vhr=00
+
+source $HOMEevs/versions/run.ver
+module reset
+module load prod_envir/${prod_envir_ver}
+source $HOMEevs/dev/modulefiles/global_det/global_det_plots.sh
+
+evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
+
+export machine=WCOSS2
+export USE_CFP=YES
+export nproc=5
+
+export envir=prod
+export NET=evs
+export STEP=plots
+export COMPONENT=global_det
+export RUN=atmos
+export VERIF_CASE=grid2grid
+export VERIF_TYPE=nwp_index
+export NDAYS=47
+
+export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
+export TMPDIR=$DATAROOT
+export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d
+today=$(cut -c7-14 ${COMROOT}/date/t${vhr}z)
+export VDATE_END=20221015
+export COMOUT=/lfs/h2/emc/ptmp/${USER}/EVS_nwp_index/$NET/$evs_ver_2d/$STEP/$COMPONENT/$RUN.$VDATE_END
+
+# Set config file
+export config=$HOMEevs/parm/evs_config/global_det/config.evs.prod.${STEP}.${COMPONENT}.${RUN}.${VERIF_CASE}.${VERIF_TYPE}
+
+# CALL executable job script here
+$HOMEevs/jobs/JEVS_GLOBAL_DET_NWP_INDEX_PLOTS
+
+######################################################################
+# Purpose: This does the plotting work for the global deterministic
+#          atmospheric grid-to-grid NWP Index for last 47 days
+######################################################################
