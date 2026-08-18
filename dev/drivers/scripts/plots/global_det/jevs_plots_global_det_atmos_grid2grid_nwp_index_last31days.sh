434408562d79fc26dace0bba0f6863994ddaaea6
shannon shields <shannon.shields@dlogin07.dogwood.wcoss2.ncep.noaa.gov>
2026-08-18 16:12:04 +0000

    Adjust memory allocation for plot jobs

--- dev/drivers/scripts/plots/global_det/jevs_plots_global_det_atmos_grid2grid_nwp_index_last31days.sh
+++ dev/drivers/scripts/plots/global_det/jevs_plots_global_det_atmos_grid2grid_nwp_index_last31days.sh
@@ -4,7 +4,7 @@
 #PBS -q dev
 #PBS -A VERF-DEV
 #PBS -l walltime=00:10:00
-#PBS -l place=vscatter:exclhost,select=1:ncpus=10:ompthreads=1:mem=150GB
+#PBS -l place=vscatter:exclhost,select=1:ncpus=10:ompthreads=1:mem=50GB
 #PBS -l debug=true
 
 set -x
